b237bde9c25e0f6248853691f46fcec3e63816cc
Sagar Bhirde <sagarbhirde111@gmail.com>
2024-05-14 16:45:22 +0530

    initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @RestController
+    public class HelloWorldController {
+
+        @GetMapping("/")
+        public String helloWorld() {
+            return "Hello, World! This is a Spring Boot Application.";
+        }
+    }
+
+}
